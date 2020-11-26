14124d51223b856a256236c8a8eb98cf0a50bec0
Miguel Branco <arlanthir@hermes.lan>
2020-11-26 00:52:03 +0000

    Add fretboard example

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/*
+document.querySelectorAll('[data-guitar]').forEach(el => {
+    if (el.dataset.guitar === 'fretboard') {
+        console.log('Adding fretboard');
+        var div = document.createElement('div');
+        div.textContent = "Sup, y'all?";
+        div.setAttribute('class', 'note');
+        el.appendChild(div);
+    }
+});
+
+*/
